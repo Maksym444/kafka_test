3257a929758dd4e11d7b284dfa74722658477976
Bradul Dmytro <dbradul@gmail.com>
2022-07-29 13:22:33 +0300

    Updated .env.example

--- docker/consumer/entrypoint.sh
+++ docker/consumer/entrypoint.sh
@@ -3,7 +3,7 @@ function gracefulShutdown {
   echo "Shutting down!"
   touch CLEAN
   python -c "open('__CLEAN__', 'w+').write('test')"
-#  python cleanup_on_exit.py
+  python cleanup_on_exit.py
 }
 trap gracefulShutdown SIGTERM
 trap gracefulShutdown SIGINT
